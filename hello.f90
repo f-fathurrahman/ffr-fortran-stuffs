a29c39374f4e1358b0b13beca473f0f96c4cf92a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-03-14 03:10:12 +0700

    Add hello.f90, the abandoned openf90 compiler could not compile it :neutral_face:, probably old incompatible library problem, time to delete OpenX64 compiler from my laptop

---
+++ hello.f90
@@ -0,0 +1,15 @@
+PROGRAM tes
+  IMPLICIT NONE
+  WRITE(*,*) 'Test ...'
+END PROGRAM
+
+SUBROUTINE test1()
+  WRITE(*,*) 'This is from subroutine test1()'
+END SUBROUTINE
+
+SUBROUTINE test2()
+  IMPLICIT NONE
+  REAL(8) :: a, b, c
+END SUBROUTINE
+
+
