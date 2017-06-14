95b441eaebeaa1a73b539c7d3841cf51c3aa1fd4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-06-15 05:41:02 +0700

    Add test_move_alloc (from gfortran doc)

--- test_move_alloc.f90
+++ test_move_alloc.f90
@@ -1,6 +1,12 @@
 PROGRAM test_move_alloc
+  IMPLICIT NONE 
+  INTEGER, ALLOCATABLE :: a(:), b(:)
 
-  IMPLICIT NONE
+  ALLOCATE(a(3))
+  a = [ 1, 2, 3 ]
+  CALL move_alloc(a, b)
+  PRINT *, allocated(a), allocated(b)
+  PRINT *, b
 
 END PROGRAM 
 
