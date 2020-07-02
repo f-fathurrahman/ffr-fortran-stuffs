d14461c8a5d621137b02ca9a2884dd5c7d6c9df0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-07-02 15:15:14 +0700

    Display info about packed MT arrays

--- elk-dft/04_display_vars.f90
+++ elk-dft/04_display_vars.f90
@@ -14,16 +14,24 @@ program test
 
   !call write_apwlo_vars()
   
+  write(*,*) "lmmaxi   ", lmmaxi
+  write(*,*) "npcmtmax ", npcmtmax
+  write(*,*) 'npmtmax  ', npmtmax
+  
   do is = 1,nspecies
     write(*,*) 'Species = ', trim(spfname(is))
-    write(*,*) 'nrsp = ', nrsp(is)
-    write(*,*) 'nrmt = ', nrmt(is)
-    write(*,*) 'rminsp = ', rminsp(is)
-    write(*,*) 'rmaxsp = ', rmaxsp(is)
     
-    write(*,*)
-    write(*,'(1x,A,5ES18.10)') 'wrmt = ', wrmt(1:5,is)
-    write(*,'(1x,A,5ES18.10)') 'wrcmt = ', wrcmt(1:5,is)
+    write(*,*) 'npmt     ', npmt(is)
+    write(*,*) 'npcmt    ', npcmt(is)
+
+    !write(*,*) 'nrsp = ', nrsp(is)
+    !write(*,*) 'nrmt = ', nrmt(is)
+    !write(*,*) 'rminsp = ', rminsp(is)
+    !write(*,*) 'rmaxsp = ', rmaxsp(is)
+    
+    !write(*,*)
+    !write(*,'(1x,A,5ES18.10)') 'wrmt = ', wrmt(1:5,is)
+    !write(*,'(1x,A,5ES18.10)') 'wrcmt = ', wrcmt(1:5,is)
 
     !write(*,'(1x,A,5ES18.10)') 'wprmt = ', wprmt(1,1:5,is)
     !write(*,*)
@@ -33,12 +41,12 @@ program test
     !write(*,*)
     !write(*,'(1x,A,5ES18.10)') 'wprmt = ', wprmt(4,1:5,is)
 
-    write(*,'(1x,A,5ES18.10)') 'wprcmt 1 = ', wprcmt(1,1:5,is)
-    write(*,'(1x,A,5ES18.10)') 'wprcmt 2 = ', wprcmt(2,1:5,is)
-    write(*,'(1x,A,5ES18.10)') 'wprcmt 3 = ', wprcmt(3,1:5,is)
-    write(*,'(1x,A,5ES18.10)') 'wprcmt 4 = ', wprcmt(4,1:5,is)
+    !write(*,'(1x,A,5ES18.10)') 'wprcmt 1 = ', wprcmt(1,1:5,is)
+    !write(*,'(1x,A,5ES18.10)') 'wprcmt 2 = ', wprcmt(2,1:5,is)
+    !write(*,'(1x,A,5ES18.10)') 'wprcmt 3 = ', wprcmt(3,1:5,is)
+    !write(*,'(1x,A,5ES18.10)') 'wprcmt 4 = ', wprcmt(4,1:5,is)
 
-    write(*,*)
+    !write(*,*)
     !write(*,'(1x,A,5ES18.10)') 'wprmt = ', wprcmt(2,1:5,is)
     !write(*,*)
     !write(*,'(1x,A,5ES18.10)') 'wprmt = ', wprcmt(3,1:5,is)
