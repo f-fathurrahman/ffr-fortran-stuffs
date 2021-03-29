9dcba4910eab23d221c002bf69b860b1ef33e442
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-03-29 11:34:22 +0700

    Test rk4

--- dftatom/plot_V_V_mid.py
+++ dftatom/plot_V_V_mid.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-matplotlib.style.use("dark_background")
-
 rmesh = np.loadtxt("fort.1000")
 Nr = rmesh.shape[0]
 V = np.loadtxt("fort.1001")
@@ -16,7 +14,8 @@ plt.clf()
 plt.plot(rmesh[idx_plot], V[idx_plot], label="V", marker="o", alpha=0.5)
 plt.plot(rmid[idx_plot], Vmid[idx_plot], label="Vmid", marker="o", linewidth=0)
 plt.legend()
-plt.savefig("IMG_V_Vmid.png", dpi=150)
+plt.grid()
+plt.savefig("IMG_V_Vmid.pdf")
 
 u1 = np.loadtxt("fort.1003")
 u2 = np.loadtxt("fort.1004")
@@ -24,5 +23,7 @@ u2 = np.loadtxt("fort.1004")
 plt.clf()
 plt.plot(rmesh, u1, label="u1")
 plt.plot(rmesh, u2, label="u2")
+plt.xlim(0, 10.0)
 plt.legend()
-plt.savefig("IMG_u1_u2.png", dpi=150)
+plt.grid()
+plt.savefig("IMG_u1_u2.pdf")
--- dftatom/test_integrate_rschroed_rk4.f90
+++ dftatom/test_integrate_rschroed_rk4.f90
@@ -3,13 +3,15 @@ PROGRAM test
   IMPLICIT NONE 
   REAL(8) :: r_min, r_max, a
   INTEGER :: Nr
-  REAL(8), ALLOCATABLE :: rmesh(:)
+  REAL(8), ALLOCATABLE :: rmesh(:), drmesh(:)
   INTEGER :: i
   REAL(8), ALLOCATABLE :: V(:), Vmid(:)
   REAL(8), ALLOCATABLE :: u1(:), u2(:)
   INTEGER :: Z, l
   INTEGER :: imax
   REAL(8) :: E
+  REAL(8) :: S
+  INTEGER :: minidx, nnodes
 
   ! Setup radial mesh
   Nr = 1000
@@ -18,7 +20,9 @@ PROGRAM test
   a = 1d9
   !a = 1.d0
   ALLOCATE( rmesh(Nr) )
+  ALLOCATE( drmesh(Nr) )
   CALL gen_rmesh_exp(r_min, r_max, a, Nr, rmesh)
+  CALL gen_drmesh_exp(r_min, r_max, a, Nr, drmesh)
 
   ! Setup potentials
   ALLOCATE( V(Nr) )
@@ -42,18 +46,67 @@ PROGRAM test
   ALLOCATE( u1(Nr) )
   ALLOCATE( u2(Nr) )
 
-  E = -0.1d0
+  E = -0.4d0
   CALL integrate_rschroed_rk4( Nr, l, Z, E, rmesh, V, Vmid, u1, u2, imax )
+  !CALL integrate_rproblem_outward( Nr, l, E, rmesh, drmesh, V, &
+  !    Z, 0.d0, .false., u1, u2, imax )
+
+  WRITE(*,*)
+  WRITE(*,*) 'After integrate_rproblem_outward'
+  WRITE(*,*) 'imax = ', imax
+  WRITE(*,*) 'Some u1 and u2'
+  DO i = 1,5
+    WRITE(*,'(1x,I8,2ES20.10)') i, u1(i), u2(i)
+  ENDDO 
+  WRITE(*,*) '....'
+  DO i = imax-5,imax
+    WRITE(*,'(1x,I8,2ES20.10)') i, u1(i), u2(i)
+  ENDDO 
+
+
+  CALL get_min_idx( imax, u1(1:imax), minidx )
+  WRITE(*,*) 'minidx = ', minidx
+
+  IF( minidx <= 0 ) THEN 
+    WRITE(1003,*) u1
+    WRITE(1004,*) u2
+    WRITE(*,*) "ERROR: The wavefunction doesn't have a peak"
+    WRITE(*,*) 'u1 and u2 are written anyway'
+    STOP
+  ENDIF 
+  
+  ! Trim the wavefunction after the last minimum:
+  !u1(minidx:) = 0.d0
+  !u2(minidx:) = 0.d0
+  
+  ! To make sure the zeros from above are not counted as nodes, we
+  ! substract 1 from minidx here:
+  CALL get_n_nodes( minidx-1, u1(1:minidx-1), nnodes )
+  WRITE(*,*) 'DEBUG: nnodes = ', nnodes
+
+  ! Normalize the wavefunction: (non-relativistic case)
+  CALL integrate_trapz_7(Nr, drmesh, u1**2, S)
+  S = sqrt(abs(S))
+  WRITE(*,*) 'normalization = ', S
+
+  !IF( S > 0.d0 ) THEN 
+  !  u1(:) = u1(:) / S
+  !  u2(:) = u2(:) / S
+  !ELSE 
+    ! This would happen if the function is zero, but we already check this
+    ! above (converged == 4), so we fail laudly here.
+  !  CALL stop_error("solve_radial_eigenproblem: zero function")
+  !ENDIF
 
   WRITE(1003,*) u1
   WRITE(1004,*) u2
 
-  WRITE(*,*) 'umax = ', imax
   WRITE(*,*) 'Pass here'
 
   DEALLOCATE( u1 )
   DEALLOCATE( u2 )
   DEALLOCATE( rmesh )
+  DEALLOCATE( drmesh )
   DEALLOCATE( V )
   DEALLOCATE( Vmid )
 
